25db9d1e8fa5e6f1963a047b81a86b3dea781d1c
xuanjituo <xuanjituo@camera360.com>
2014-06-08 22:54:06 +0800

    apk signe to release

--- src/com/xjt/letool/common/LLog.java
+++ src/com/xjt/letool/common/LLog.java
@@ -1,59 +1,84 @@
+
 package com.xjt.letool.common;
 
 public class LLog {
+
+    private static boolean DEBUG = false;
     private static final String TAG = "letooltag:";
 
     public static int v(String tag, String msg) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.v(tag, msg);
     }
 
     public static int v(String tag, String msg, Throwable tr) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.v(tag, msg, tr);
     }
 
     public static int d(String tag, String msg) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.d(tag, msg);
     }
 
     public static int d(String tag, String msg, Throwable tr) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.d(tag, msg, tr);
     }
 
     public static int i(String tag, String msg) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.i(tag, msg);
     }
 
     public static int i(String tag, String msg, Throwable tr) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.i(tag, msg, tr);
     }
 
     public static int w(String tag, String msg) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.w(tag, msg);
     }
 
     public static int w(String tag, String msg, Throwable tr) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.w(tag, msg, tr);
     }
 
     public static int w(String tag, Throwable tr) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.w(tag, tr);
     }
 
     public static int e(String tag, String msg) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.e(tag, msg);
     }
 
     public static int e(String tag, String msg, Throwable tr) {
+        if (!DEBUG)
+            return 0;
         tag = TAG + tag;
         return android.util.Log.e(tag, msg, tr);
     }
--- src/com/xjt/letool/stat/StatConstants.java
+++ src/com/xjt/letool/stat/StatConstants.java
@@ -7,6 +7,27 @@ public class StatConstants {
     public static final String UMENG_APP_DEBUG_KEY = "538f2c0456240ba4a0059794";
 
     public static final String UMENG_CHANNEL_ID = "10000";
+    public static final String UMENG_CHANNEL_ID_WDJ = "LETOOL0000001000"; //豌豆夹
+    public static final String UMENG_CHANNEL_91 = "LETOOL0000001001"; //91手机助手
+    public static final String UMENG_CHANNEL_TENCENT ="LETOOL0000001002";//腾讯应用宝贝
+    public static final String UMENG_CHANNEL_BAIDU ="LETOOL0000001003";//百度
+    public static final String UMENG_CHANNEL_360 = "LETOOL0000001004";//360手机助手
+    public static final String UMENG_CHANNEL_ALI = "LETOOL0000001005";//阿里
+    public static final String UMENG_CHANNEL_ANZHI = "LETOOL0000001006";//安智
+    public static final String UMENG_CHANNEL_MUMAYI = "LETOOL0000001007";//木蚂蚁
+    public static final String UMENG_CHANNEL_ANZHUO = "LETOOL0000001009";//安卓
+    public static final String UMENG_CHANNEL_XIAOMI ="LETOOL0000001010";//小米
+    public static final String UMENG_CHANNEL_JIFENG ="LETOOL0000001011";//机峰
+    public static final String UMENG_CHANNEL_YINGYONGHUI = "LETOOL0000001012";//应用汇
+    public static final String UMENG_CHANNEL_NDUO ="LETOOL0000001013";//N多
+    public static final String UMENG_CHANNEL_EOE = "LETOOL0000001014";//EOE
+    public static final String UMENG_CHANNEL_UC = "LETOOL0000001015"; //UC
+    public static final String UMENG_CHANNEL_DIANXIN = "LETOOL0000001016";//电信
+    public static final String UMENG_CHANNEL_SANXING = "LETOOL0000001017";//三星
+    public static final String UMENG_CHANNEL_LIANXIANG = "LETOOL0000001018";//联想
+    public static final String UMENG_CHANNEL_HUAWEI = "LETOOL0000001019";//华为
+    public static final String UMENG_CHANNEL_ZHONGXIN = "LETOOL0000001020"; //中兴
+    public static final String UMENG_CHANNEL_YIDONGMM = "LETOOL0000001021";//移动MM
 
     public static final String EVENT_KEY_SLIDE_MENU = "slid_menu";
     public static final String EVENT_KEY_SLIDE_MENU_MENU = "slid_menu_menu";
