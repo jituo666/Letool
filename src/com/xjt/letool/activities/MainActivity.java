e1e613dbf98ed1af7a3be81684dc93c78dc8e359
xuanjituo <xuanjituo@camera360.com>
2014-08-02 13:43:46 +0800

    fix umeng stat code

---
+++ src/com/xjt/letool/activities/MainActivity.java
@@ -0,0 +1,23 @@
+
+package com.xjt.letool.activities;
+
+import android.os.Bundle;
+
+import com.umeng.analytics.AnalyticsConfig;
+import com.xjt.letool.stat.StatConstants;
+
+/**
+ * @Author Jituo.Xuan
+ * @Date 1:38:59 PM Aug 2, 2014
+ * @Comments:null
+ */
+public class MainActivity extends LocalMediaActivity {
+
+    @Override
+    protected void onCreate(Bundle b) {
+        AnalyticsConfig.setAppkey(StatConstants.UMENG_APP_DEBUG_KEY);
+        AnalyticsConfig.setChannel(StatConstants.UMENG_TEST_CHANNEL_ID);
+        super.onCreate(b);
+    }
+
+}
