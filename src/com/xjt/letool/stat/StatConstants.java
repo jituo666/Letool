9e61c2adc352acf415b7f1238220985dd59897d0
xuanjituo <xuanjituo@camera360.com>
2014-08-02 14:43:47 +0800

    add ument channel id

--- src/com/xjt/letool/stat/StatConstants.java
+++ src/com/xjt/letool/stat/StatConstants.java
@@ -9,8 +9,33 @@ package com.xjt.letool.stat;
 public class StatConstants {
 
     public static final String UMENG_APP_RELEASE_KEY = "539289e956240b9f6902fa3c";
-    public static final String UMENG_APP_DEBUG_KEY = "538f2c0456240ba4a0059794";
 
+    public static final String UMENG_CHANNEL_ID_WDJ = "LETOOL0000001000"; //豌豆夹
+    public static final String UMENG_CHANNEL_91 = "LETOOL0000001001"; //91手机助手
+    public static final String UMENG_CHANNEL_TENCENT = "LETOOL0000001002";//腾讯应用宝
+    public static final String UMENG_CHANNEL_BAIDU = "LETOOL0000001003";//百度
+    public static final String UMENG_CHANNEL_360 = "LETOOL0000001004";//360手机助手
+    public static final String UMENG_CHANNEL_ALI = "LETOOL0000001005";//阿里
+    public static final String UMENG_CHANNEL_ANZHI = "LETOOL0000001006";//安智
+    public static final String UMENG_CHANNEL_MUMAYI = "LETOOL0000001007";//木蚂蚁
+    public static final String UMENG_CHANNEL_ANZHUO = "LETOOL0000001008";//安卓
+    public static final String UMENG_CHANNEL_XIAOMI = "LETOOL0000001009";//小米
+    public static final String UMENG_CHANNEL_JIFENG = "LETOOL0000001010";//机峰
+    public static final String UMENG_CHANNEL_YINGYONGHUI = "LETOOL0000001011";//应用汇
+    public static final String UMENG_CHANNEL_NDUO = "LETOOL0000001012";//N多
+    public static final String UMENG_CHANNEL_EOE = "LETOOL0000001013";//EOE
+    public static final String UMENG_CHANNEL_UC = "LETOOL0000001014"; //UC
+    public static final String UMENG_CHANNEL_DIANXIN = "LETOOL0000001015";//电信
+    public static final String UMENG_CHANNEL_SANXING = "LETOOL0000001017";//三星
+    public static final String UMENG_CHANNEL_LIANXIANG = "LETOOL0000001018";//联想
+    public static final String UMENG_CHANNEL_HUAWEI = "LETOOL0000001019";//华为
+    public static final String UMENG_CHANNEL_ZHONGXIN = "LETOOL0000001020"; //中兴
+    public static final String UMENG_CHANNEL_YIDONGMM = "LETOOL0000001021";//移动MM
+    public static final String UMENG_CHANNEL_WOSHOP = "LETOOL0000001022";//联通沃商店
+    public static final String UMENG_CHANNEL_OTHERS = "LETOOL0000001111";//其它
+
+    //
+    public static final String UMENG_APP_DEBUG_KEY = "538f2c0456240ba4a0059794";
     public static final String UMENG_TEST_CHANNEL_ID = "com-xjt-letool-umeng-test-id";
 
     //
